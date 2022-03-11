ea454ed366b3bbf65e73249865067560f5e8ced0
raihanmd <mraihanf471@gmail.com>
2022-03-11 15:19:24 +0700

    changing from cdn to cli tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./index.html'],
+  darkMode: 'class',
+  theme: {
+      extend: {
+      },
+      fontFamily: {
+          comfortaa: ['Comfortaa', 'cursive'],
+          fredoka: ['Fredoka', 'sans-serif']
+      },
+  },
+  plugins: [],
+}
